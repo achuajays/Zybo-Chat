c8967ee37c00300fa5d88deb7102371819493503
Adarsh Ajay <adarshajays2003@gmail.com>
2026-02-18 19:44:36 +0530

    feat: Implement the initial Register screen UI with form inputs and branding.

--- config/settings.py
+++ config/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "core",
+    'channels',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
+ASGI_APPLICATION = 'config.asgi.application'
+
+# Channel Layers
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    },
+}
 
 
 # Database
@@ -116,8 +126,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom User Model
+AUTH_USER_MODEL = 'core.User'
+
+# Authentication
+AUTHENTICATION_BACKENDS = [
+    'core.backends.EmailBackend',
+]
+
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/users/'
---
+++ core/backends.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.backends import ModelBackend
+from .models import User
+
+
+class EmailBackend(ModelBackend):
+    """Authenticate using email instead of username."""
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(email=username)
+        except User.DoesNotExist:
+            return None
+
+        if user.check_password(password):
+            return user
+        return None
